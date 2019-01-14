5570f9858d41f644d98fdf6cc4e31881fe0145e1
Tien Le and Travis Toal <tien+travis@>
2019-01-14 15:58:36 -0800

    add style and pizazz

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,6 @@ function add(num1, num2) {
 	return num1 + num2;
 }
 
-
-
 function subtract(num1, num2) {
 	return num1 - num2;
 }
@@ -17,23 +15,29 @@ function divide(num1, num2) {
 	return num1 / num2;
 }
 
-var choice = parseInt(prompt("Would you like to\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n?: "));
-
-var number1 = parseInt(prompt("Enter a number: "));
-var number2 = parseInt(prompt("Enter a second number: "));
-
-if (choice === 1) {
-  alert(number1 + " plus " + number2 + " is " + add(number1, number2));
-}
-else if (choice === 2) {
-  alert(number1 + " minus " + number2 + " is " + subtract(number1, number2));
-}
-else if (choice === 3) {
-  alert(number1 + " times " + number2 + " is " + ultiply(number1, number2));
-}
-else if (choice === 4) {
-  alert(number1 + " divided by " + number2 + " is " + divide(number1, number2));
-}
-else {
-  alert("That is not a valid choice; goodbye.");
-}
+var choice;
+var number1;
+var number2;
+$("#calculate").click(function() {
+  var choice = prompt("Would you like to\nAdd\nSubtract\nMultiply\nDivide\n?: ");
+  var number1 = parseInt(prompt("Enter a number: "));
+  var number2 = parseInt(prompt("Enter a second number: "));
+  if (choice === "add") {
+    document.write(number1 + " plus " + number2 + " is " + add(number1, number2));
+  }
+  else if (choice === "subtract") {
+    document.write(number1 + " minus " + number2 + " is " + subtract(number1, number2));
+  }
+  else if (choice === "multiply") {
+    document.write(number1 + " times " + number2 + " is " + multiply(number1, number2));
+  }
+  else if (choice === "divide") {
+    document.write(number1 + " divided by " + number2 + " is " + divide(number1, number2));
+  }
+  else {
+    alert("That is not a valid choice; goodbye.");
+  }
+});
+choice = "";
+number1 = 0;
+number2 = 0;
